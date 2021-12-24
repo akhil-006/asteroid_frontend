5925a752f9d0bfc2795e13ff236ce6e49dfa398c
akhil agarwal <akhil_agarwal006@yahoo.in>
2021-12-24 21:55:32 +0530

    added docstrings to redis package.

--- asteroidtrackingBE/redis_pkg/conn_handlers.py
+++ asteroidtrackingBE/redis_pkg/conn_handlers.py
@@ -3,6 +3,10 @@ from redis import exceptions
 
 
 def connect(host='127.0.0.1', port=6379, pwd=None):
+    """
+    Redis Connection handler which helps in getting redis connection pointer. This function tries to get the redis
+    connection 3 times and breaks if the connection is not acquired.
+    """
     rconn = Redis(host=host, port=port)
     retries, retry_conn_counter = 0, 3
     while not rconn:
--- asteroidtrackingBE/redis_pkg/redis_library.py
+++ asteroidtrackingBE/redis_pkg/redis_library.py
@@ -1,19 +1,36 @@
 
 def add_data_to_stream(rconn, stream, data):
+    """
+    Adds the `data` to the (supplied)redis `stream` with the help of redis connection handler
+    """
     rconn.xadd(name=stream, fields=data)
 
 
 def read_data_from_stream(rconn, stream, count, block):
+    """
+    reads the data from the (supplied)redis `stream` with the help of redis connection handler. The amount of data read
+    is dependent on the value of `count` and the `stream` for `block` milliseconds before a data is read.
+    Note: Always a new data is read since the ID is taken as '$'.
+    """
     return rconn.xread({stream: '$'}, count, block)
 
 
-def set_data(rconn, key, value, config='config:asteroid'):
+def set_data(rconn, key, value, config='info:asteroid'):
+    """
+    Sets the data in redis hash. In python it is read as config[key] = value
+    """
     rconn.hset(config, key, value)
 
 
-def get_data(rconn, key, config='config:asteroid'):
+def get_data(rconn, key, config='info:asteroid'):
+    """
+    Gets the data from the redis hash
+    """
     return rconn.hget(config, key)
 
 
-def delete_data(rconn, key, config='config:asteroid'):
+def delete_data(rconn, key, config='info:asteroid'):
+    """
+    Deletes the key from the redis hash
+    """
     return rconn.hdel(config, key)
